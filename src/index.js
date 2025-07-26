33a443e3f24e035c5444927b83ea90e4999c9e19
Thomas David <tom@mightysmile.com>
2025-07-26 12:50:39 +1000

    feat: inline-css plugin now uses postcss for css processing

--- src/index.js
+++ src/index.js
@@ -1,53 +1,85 @@
 import { createPlugin, } from 'coralite/utils'
-import { readFile, access } from 'node:fs/promises'
-import { resolve } from 'node:path';
+import { readFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import postcss from 'postcss'
+import postcssAtImport from 'postcss-import'
+import postcssMinify from 'postcss-minify'
 
 /**
- * @import {CoraliteAnyNode, CoraliteDirective, CoraliteDocumentRoot} from 'coralite/types'
+ * @import {
+ *  CoraliteAnyNode,
+ *  CoraliteDirective,
+ *  CoraliteDocumentRoot
+ * } from 'coralite/types'
  */
 
-export default createPlugin({
-  name: 'inline-css',
-  async onPageSet (context) {
-    /** @type {(CoraliteAnyNode | CoraliteDocumentRoot | CoraliteDirective)[]} */
-    let stack = [context.elements.root];
+/**
+ * Coralite plugin to inline CSS
+ * @param {Object} [options] - Plugin options
+ * @param {boolean} [options.atImport] - Whether to use postcss-at-import plugin
+ * @param {boolean} [options.minify] - Whether to minify CSS
+ */
+export default ({
+  atImport,
+  minify
+} = {}) => {
+  const css = postcss()
 
-    while (stack.length > 0) {
-      const currentNode = stack.pop();
+  if (atImport) {
+    css.use(postcssAtImport())
+  }
 
-      // check if the current node is a <link> tag with rel="stylesheet" and inline-css attribute
-      if (currentNode.type === 'tag'
-        && currentNode.name === 'link'
-        && typeof currentNode.attribs === 'object'
-        && currentNode.attribs.rel === 'stylesheet'
-        && currentNode.attribs['inline-css'] != null) {
+  if (minify) {
+    css.use(postcssMinify)
+  }
 
-        // resolve the CSS file path using the inline-css attribute or href
-        const cssPath = resolve(currentNode.attribs['inline-css'] || currentNode.attribs.href);
+  return createPlugin({
+    name: 'inline-css',
+    async onPageSet (context) {
+      /** @type {(CoraliteAnyNode | CoraliteDocumentRoot | CoraliteDirective)[]} */
+      let stack = [context.elements.root];
 
-        try {
-          // read the CSS content from the resolved path
-          const data = await readFile(cssPath, { encoding: 'utf8' });
-          
-          // replace the <link> tag with a <style> tag and set its content
-          currentNode.name = 'style';
-          currentNode.attribs = {};
-          currentNode.children = [
-            {
-              type: 'text',
-              data,
-              parent: currentNode
-            }
-          ];
-        } catch (err) {
-          console.error(err);
+      while (stack.length > 0) {
+        const currentNode = stack.pop();
+
+        // check if the current node is a <link> tag with rel="stylesheet" and inline-css attribute
+        if (currentNode.type === 'tag'
+          && currentNode.name === 'link'
+          && typeof currentNode.attribs === 'object'
+          && currentNode.attribs.rel === 'stylesheet'
+          && currentNode.attribs['inline-css'] != null) {
+
+          // resolve the CSS file path using the inline-css attribute or href
+          const cssPath = resolve(currentNode.attribs['inline-css'] || currentNode.attribs.href);
+
+          try {
+            // read the CSS content from the resolved path
+            const data = await readFile(cssPath, { encoding: 'utf8' });
+            const result = await css.process(data, {
+              // `from` option is needed here
+              from: cssPath
+            })
+
+            // replace the <link> tag with a <style> tag and set its content
+            currentNode.name = 'style';
+            currentNode.attribs = {};
+            currentNode.children = [
+              {
+                type: 'text',
+                data: result.css,
+                parent: currentNode
+              }
+            ];
+          } catch (err) {
+            console.error(err);
+          }
+        }
+
+        // add children to the stack for further processing
+        if ((currentNode.type === 'tag' || currentNode.type === 'root') && currentNode.children) {
+          stack = stack.concat(currentNode.children);
         }
       }
-
-      // add children to the stack for further processing
-      if ((currentNode.type === 'tag' || currentNode.type === 'root') && currentNode.children) {
-        stack = stack.concat(currentNode.children);
-      }
     }
-  }
-})
+  })
+}
