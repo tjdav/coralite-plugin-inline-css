27c1688ce032344ec6b8b57c1056d81b50acabf1
Thomas David <tom@mightysmile.com>
2025-07-26 23:11:16 +1000

    test: cover support for importing and minifying CSS in inline CSS plugin

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,12 +5,12 @@ import { strictEqual, notStrictEqual, ok } from 'node:assert/strict'
 import Coralite from 'coralite'
 import inlineCSS from '../src/index.js' // Adjust path if needed
 
-test('setup coralite with inline-css plugin', async () => {
+test('default inline css', async () => {
   // Setup configuration
   const config = {
     templates: 'test/fixtures/templates',
     pages: 'test/fixtures/pages',
-    plugins: [inlineCSS]
+    plugins: [inlineCSS()]
   }
 
   // Create Coralite instance
@@ -27,9 +27,71 @@ test('setup coralite with inline-css plugin', async () => {
   strictEqual(documents.length, 1) // Assuming one document is expected
 
   // Expected inline CSS content
-  const expectedCSS = `  <style>body {
+  const expectedCSS = `<style>body {
   background-color: rebeccapurple;
   color: white;
+}</style>`
+
+  // Basic check for inlined styles
+  ok(documents[0].html.includes(expectedCSS))
+})
+
+test('minified inline css', async () => {
+  // Setup configuration
+  const config = {
+    templates: 'test/fixtures/templates',
+    pages: 'test/fixtures/pages',
+    plugins: [inlineCSS({
+      minify: true
+    })]
+  }
+
+  // Create Coralite instance
+  const coralite = new Coralite(config)
+
+  // Initialize the Coralite instance
+  await coralite.initialise()
+
+  // Compile documents
+  const documents = await coralite.compile()
+
+  // Verify that documents were compiled
+  notStrictEqual(documents, undefined)
+  strictEqual(documents.length, 1) // Assuming one document is expected
+
+  // Expected inline CSS content
+  const expectedCSS = `<style>body{background-color:rebeccapurple;color:white}</style>`
+
+  // Basic check for inlined styles
+  ok(documents[0].html.includes(expectedCSS))
+})
+
+test('inline @import css', async () => {
+  // Setup configuration
+  const config = {
+    templates: 'test/fixtures/templates',
+    pages: 'test/fixtures/pages',
+    plugins: [inlineCSS({
+      atImport: true
+    })]
+  }
+
+  // Create Coralite instance
+  const coralite = new Coralite(config)
+
+  // Initialize the Coralite instance
+  await coralite.initialise()
+
+  // Compile documents
+  const documents = await coralite.compile()
+
+  // Verify that documents were compiled
+  notStrictEqual(documents, undefined)
+  strictEqual(documents.length, 1) // Assuming one document is expected
+
+  // Expected inline CSS content
+  const expectedCSS = `<style>p {
+  color: red;
 }</style>`
 
   // Basic check for inlined styles
